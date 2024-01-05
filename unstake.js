31130dbf1c2268ff29d7d3a4922b90b6d40dc47d
Eric Chen <ericlchen2@gmail.com>
2024-01-05 09:41:52 -0800

    Adding websocket redeem event

--- unstake.js
+++ unstake.js
@@ -20,7 +20,7 @@ const apiKey = process.env.API_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS.toLowerCase();
 const FIREBLOCKS_ADDRESS = "0x2e9E16e6deB4022399E4FCd387bCB59Ac5855762";
 const walletAddress = process.env.WALLET_ADDRESS.toLowerCase();
-
+const INFURA_SECRET = process.env.INFURA_SECRET;
 
 const ABI = require("./ContractTest.json").abi;
 
@@ -111,6 +111,26 @@ const createRedeemRequest = async () => {
 }
 // createRedeemRequest();
 
+
+const checkRedeemEvent = async () => {
+    //insert secret here below. Replace Goerli with any other
+    const provider = new ethers.providers.WebSocketProvider(
+        `wss://goerli.infura.io/ws/v3/${INFURA_SECRET}`, "goerli"
+      );
+    
+    const redeemManagerContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider.getSigner());
+
+    redeemManagerContract.on("RequestedRedeem",(owner, height, amount, maxRedeemableEth, id) => {
+      console.log('RequestedRedeem event');
+      console.log(`Owner of redeem ${owner}`)
+      console.log(`Height ${height.toString()}`);
+      console.log(`Amount of LsETH to redeem ${amount.toString()}`);
+      console.log(`Maximum amount of ETH to redeem ${maxRedeemableEth.toString()}`);
+      console.log(`Request Redeem ID ${id}`);
+    });
+}
+// checkRedeemEvent
+
 const resolveRedeemRequest = async () => { 
 
     const provider = new ethers.providers.Web3Provider(eip1193Provider);
@@ -141,3 +161,4 @@ const createClaimRedeemRequest = async () => {
 
 }
 //createClaimRedeemRequest();
+
